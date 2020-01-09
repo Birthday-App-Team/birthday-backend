ebb659af679cbf9e7e7597ef4283b59eb1e2cea8
Marsha <marshaleonoradunn@gmail.com>
2020-01-09 21:16:39 +0000

    removed axios from backend

--- handler.js
+++ handler.js
@@ -4,14 +4,12 @@ const serverlessHttp = require("serverless-http");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const mysql = require("mysql");
-const moment = require("moment");
-const axios = require("axios");
 
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: "birthdaydb",
+  database: "birthdaydb"
 });
 
 const app = express();
@@ -34,19 +32,19 @@ app.delete("/birthdays/:birthdayID", (req, res) => {
   connection.query(
     "DELETE FROM Birthdays WHERE birthdayID = ?",
     [birthdayID],
-    (err) => {
+    err => {
       if (err) {
         res.status(500).json({ error: err });
       } else {
         res.status(200).send(`${birthdayID} has been deleted!`);
       }
-    },
+    }
   );
 });
 
 app.post("/birthdays", (req, res) => {
   const birthday = req.body;
-  // install and require moment
+  const dob = birthday; // install and require moment
   // find out if it is this person's birthday today
   // if so, make an axios request to the sendBirthday message function
   const q = "INSERT INTO Birthdays SET ?";
@@ -54,31 +52,8 @@ app.post("/birthdays", (req, res) => {
     if (err) {
       res.status(500).json({ error: err });
     } else {
-      // eslint-disable-next-line no-lonely-if
-      if (moment(birthday).format("MM-DD") === moment().format("MM-DD")) {
-        axios
-          .post(
-            "https://46m3x72wmb.execute-api.eu-west-2.amazonaws.com/dev/send",
-            {
-              recipient_name: birthday.name,
-              recipient_phone_number: birthday.phone_number,
-              message: "What a lovely morning!:D :D",
-              from_phone_number: "+447506190696"
-            }
-          )
-          .then(() => {
-            birthday.birthdayID = data.insertID;
-            res.status(201).json(birthday);
-          })
-          .catch(err => {
-            console.log("Error sending birthday message", err);
-            birthday.birthdayID = data.insertID;
-            res.status(201).send(birthday);
-          });
-      } else {
-        birthday.birthdayID = data.insertID;
-        res.status(201).json(birthday);
-      }
+      birthday.birthdayID = data.insertID;
+      res.status(201).json(birthday);
     }
   });
 });
@@ -98,7 +73,7 @@ app.put("/birthdays/:birthdayID", (req, res) => {
       birthday.phone_number,
       birthdayID
     ],
-    (err) => {
+    (err, data) => {
       if (err) {
         res.status(500).json({ error: err });
       } else {
@@ -106,11 +81,11 @@ app.put("/birthdays/:birthdayID", (req, res) => {
           .status(205)
           .send(
             `You updated a birthday for ${
-              birthday.name
+            birthday.name
             } with the following data: ${JSON.stringify(birthday)}`
           );
       }
-    },
+    }
   );
 });
 
